bdaba693fa6f3d5ce6d2541d4ba65ac23a3ebccb
MAGDX <magnum_dx@hotmail.com>
2019-07-22 08:39:42 +0200

    Cambio en metodo crear: añadido condicion affected rows

--- src/main/java/com/ipartek/formacion/model/dao/VideoDAO.java
+++ src/main/java/com/ipartek/formacion/model/dao/VideoDAO.java
@@ -10,7 +10,7 @@ import com.ipartek.formacion.model.ConnectionManager;
 import com.ipartek.formacion.model.pojo.Video;
 
 public class VideoDAO {
-		
+
 	private static VideoDAO INSTANCE = null;
 
 	private VideoDAO() {
@@ -25,7 +25,7 @@ public class VideoDAO {
 	}
 
 	public ArrayList<Video> getAll() {
-		
+
 		ArrayList<Video> lista = new ArrayList<Video>();
 		String sql = "SELECT `id`, `nombre`, `codigo` FROM `video` ORDER BY `id` DESC LIMIT 500";
 
@@ -35,9 +35,9 @@ public class VideoDAO {
 
 			while (rs.next()) {
 				Video v = new Video();
-				v.setId( rs.getInt("id") );
-				v.setNombre( rs.getString("nombre"));
-				v.setCodigo( rs.getString("codigo"));
+				v.setId(rs.getInt("id"));
+				v.setNombre(rs.getString("nombre"));
+				v.setCodigo(rs.getString("codigo"));
 				lista.add(v);
 			}
 		} catch (Exception e) {
@@ -46,13 +46,12 @@ public class VideoDAO {
 		}
 		return lista;
 	}
-	
+
 	public Video getById(int id) {
 		Video video = new Video();
 		String sql = "SELECT `id`, `nombre`, `codigo` FROM `video` WHERE `id` = ?;";
 
-		try (Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(sql)) {
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(sql)) {
 			pst.setInt(1, id);
 			try (ResultSet rs = pst.executeQuery()) {
 				if (rs.next()) {
@@ -66,51 +65,50 @@ public class VideoDAO {
 		}
 		return video;
 	}
-	
+
 	public boolean crear(Video pojo) {
 		boolean resultado = false;
 		String sql = "INSERT INTO `video` (`nombre`, `codigo`) VALUES (?,?);";
 
-		try (Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(sql)) {
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(sql)) {
 
 			pst.setString(1, pojo.getNombre());
 			pst.setString(2, pojo.getCodigo());
-			pst.executeUpdate();
 
+			if (pst.executeUpdate() == 1) {
+				resultado = true;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return resultado;
 	}
-	
+
 	public boolean modificar(Video pojo) {
 		boolean resultado = false;
 
 		String sql = "UPDATE `video` SET `nombre`= ?, `codigo`= ? WHERE `id`= ?;";
 
-		try (Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(sql)) {
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(sql)) {
 
 			pst.setString(1, pojo.getNombre());
 			pst.setString(2, pojo.getCodigo());
 			pst.setInt(3, pojo.getId());
 			pst.executeUpdate();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return resultado;
 	}
-	
+
 	public boolean delete(int id) {
 		boolean resultado = false;
 		String sql = "DELETE FROM `video` WHERE `id`= ?;";
 
-		try (Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(sql);) {
-			
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(sql);) {
+
 			pst.setInt(1, id);
 
 			int affetedRows = pst.executeUpdate();
@@ -124,59 +122,37 @@ public class VideoDAO {
 
 		return resultado;
 	}
-/*
-	private boolean doSave(PreparedStatement pst, Video pojo){
-		boolean resultado = false;
-
-		try {
-			int affectedRows = pst.executeUpdate();
-			if (affectedRows == 1) {
-				try (ResultSet generatedKeys = pst.getGeneratedKeys()) {
-					if (generatedKeys.next()) {
-						pojo.setId(generatedKeys.getInt(1));
-					}
-				}
-				resultado = true;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return resultado;
-	}
-	
-	public ArrayList<Rol> getByName(String search) {
-
-		ArrayList<Rol> lista = new ArrayList<Rol>();
-		String sql = "SELECT id, nombre FROM rol WHERE nombre LIKE ? ORDER BY id DESC LIMIT 500;";
-
-		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(sql)) {
-			pst.setString(1, "%" + search + "%");
-			try (ResultSet rs = pst.executeQuery()) {
-				while (rs.next()) {
-					lista.add(mapper(rs));
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return lista;
-	}
-	
-	public boolean save(Rol pojo) throws SQLException {
-		boolean resultado = false;
-
-		if (pojo != null) {
-			// Sanitize nombre
-			pojo.setNombre(Utilidades.limpiarEspacios(pojo.getNombre()));
-
-			if (pojo.getId() == -1) {
-				resultado = crear(pojo);
-			} else {
-				resultado = modificar(pojo);
-			}
-		}
-
-		return resultado;
-	}*/
+	/*
+	 * private boolean doSave(PreparedStatement pst, Video pojo){ boolean resultado
+	 * = false;
+	 * 
+	 * try { int affectedRows = pst.executeUpdate(); if (affectedRows == 1) { try
+	 * (ResultSet generatedKeys = pst.getGeneratedKeys()) { if
+	 * (generatedKeys.next()) { pojo.setId(generatedKeys.getInt(1)); } } resultado =
+	 * true; } } catch (Exception e) { e.printStackTrace(); }
+	 * 
+	 * return resultado; }
+	 * 
+	 * public ArrayList<Rol> getByName(String search) {
+	 * 
+	 * ArrayList<Rol> lista = new ArrayList<Rol>(); String sql =
+	 * "SELECT id, nombre FROM rol WHERE nombre LIKE ? ORDER BY id DESC LIMIT 500;";
+	 * 
+	 * try (Connection con = ConnectionManager.getConnection(); PreparedStatement
+	 * pst = con.prepareStatement(sql)) { pst.setString(1, "%" + search + "%"); try
+	 * (ResultSet rs = pst.executeQuery()) { while (rs.next()) {
+	 * lista.add(mapper(rs)); } } } catch (Exception e) { e.printStackTrace(); }
+	 * return lista; }
+	 * 
+	 * public boolean save(Rol pojo) throws SQLException { boolean resultado =
+	 * false;
+	 * 
+	 * if (pojo != null) { // Sanitize nombre
+	 * pojo.setNombre(Utilidades.limpiarEspacios(pojo.getNombre()));
+	 * 
+	 * if (pojo.getId() == -1) { resultado = crear(pojo); } else { resultado =
+	 * modificar(pojo); } }
+	 * 
+	 * return resultado; }
+	 */
 }
